dbfc0212f775fa3a645e2ba5ccee304b66fef788
Logan Nguyen <36346192+NBaLogn@users.noreply.github.com>
2026-01-28 14:42:03 +0700

    misc

---
+++ whisper.sh
@@ -0,0 +1,8 @@
+./build/bin/whisper-cli \
+    --file '/Users/logan/Downloads/Fragile - Tatsuro Yamashita ⧸ Miori Celesta [qNFBzyzqZdc].wav' \
+    --model models/ggml-large-v3.bin \
+    --print-colors 
+
+    # -owts
+    # --vad \
+    # --vad-model ./models/silero-v6.2.0-ggml.bin \
